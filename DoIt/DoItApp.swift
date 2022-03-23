3ba13a07021386642b0a1dc554feaeb45effeeb2
David Kim <david.kim@richmond.edu>
2022-03-22 21:13:09 -0400

    Initial Commit

---
+++ DoIt/DoItApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoItApp.swift
+//  DoIt
+//
+//  Created by Dong Kyun Kim on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct DoItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
